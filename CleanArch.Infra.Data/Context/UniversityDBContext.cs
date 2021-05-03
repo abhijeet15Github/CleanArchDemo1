0b2c9bb937936d86f9ef9c5ae7df45aea6d44d7a
Abhijeet Nevaskar <abhijeet.nevaskar@gmail.com>
2021-05-03 12:42:25 +0530

    added infra.data & domain

---
+++ CleanArch.Infra.Data/Context/UniversityDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using CleanArch.Domain.Models;
+
+namespace CleanArch.Infra.Data.Context
+{
+    public class UniversityDBContext : DbContext 
+    {
+        public UniversityDBContext( DbContextOptions options ) : base (options)
+        {
+
+        }
+
+        public DbSet<Course> Courses { get; set; }
+
+    }
+}

1d97f55df1b835a8a8a43e6b9d7e23ad14830436
Abhijeet Nevaskar <abhijeet.nevaskar@gmail.com>
2021-05-05 21:10:45 +0530

    final changes

---
+++ CleanArch.MVC/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}

27539927a2249a0ea046474b8e90f226d25148e2
Abhijeet Nevaskar <abhijeet.nevaskar@gmail.com>
2021-05-06 23:22:25 +0530

    service implementation

--- CleanArch.Application/Services/CourseService.cs
+++ CleanArch.Application/Services/CourseService.cs
@@ -1,20 +1,34 @@
-﻿using System;
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
+using CleanArch.Domain.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using CleanArch.Application.Interfaces;
-using CleanArch.Application.ViewModels;
-using CleanArch.Domain.Interfaces;
 
 namespace CleanArch.Application.Services
 {
     public class CourseService : ICourseService
     {
+        private readonly ICourseRepository _courseRepository;
+        private readonly IMediatorHandler _bus;
 
-        private ICourseRepository _courseRepository;
-
-        public CourseService(ICourseRepository courseRepository)
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
+        }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                  courseViewModel.Name,
+                  courseViewModel.Description,
+                  courseViewModel.ImageUrl
+                );
+
+            _bus.SendCommand(createCourseCommand);
         }
 
         public CourseViewModel GetCourses()
--- CleanArch.Application/ViewModels/CourseViewModel.cs
+++ CleanArch.Application/ViewModels/CourseViewModel.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using CleanArch.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
-using CleanArch.Domain.Models;
 
 namespace CleanArch.Application.ViewModels
 {
     public class CourseViewModel
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
         public IEnumerable<Course> Courses { get; set; }
     }
 }
